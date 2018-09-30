c0c047e63d633cb416749b8d6ecc7f47cab0ea51
Abhinav Sharma <abhi18av@users.noreply.github.com>
2018-09-30 20:34:24 +0530

    Update analysis.sh

--- analysis.sh
+++ analysis.sh
@@ -1,16 +1,16 @@
 # gzip these files
 
-gzip -dc G04868_R1.fastq.gz > G04868_R1.fastq
+gzip -dc MAFBRA00707_R1.fastq.gz > MAFBRA00707_R1.fastq
 
 
-gzip -dc G04868_R2.fastq.gz > G04868_R2.fastq
+gzip -dc MAFBRA00707_R2.fastq.gz > MAFBRA00707_R2.fastq
 
 
 
 # trimmomatic <<<<<
 
 
-java -jar /opt/Trimmomatic-0.36/trimmomatic-0.36.jar PE -phred33 G04868_1.fastq G04868_2.fastq G04868_1_trimmed_paired.fastq G04868_1_trimmed_unpaired.fastq G04868_2_trimmed_paired.fastq G04868_2_trimmed_unpaired.fastq LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:36
+java -jar /opt/Trimmomatic-0.36/trimmomatic-0.36.jar PE -phred33 MAFBRA00707_1.fastq MAFBRA00707_2.fastq MAFBRA00707_1_trimmed_paired.fastq MAFBRA00707_1_trimmed_unpaired.fastq MAFBRA00707_2_trimmed_paired.fastq MAFBRA00707_2_trimmed_unpaired.fastq LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:36
 
 
 
@@ -25,7 +25,7 @@ bwa index NC000962_3.fasta
 # map_and_generate_sam_file <<<<<
 
 
-bwa mem -R "@RG\tID:G04868\tSM:G04868\tPL:Illumina" -M NC000962_3.fasta G04868_1_trimmed_paired.fastq G04868_2_trimmed_paired.fastq > G04868.sam
+bwa mem -R "@RG\tID:MAFBRA00707\tSM:MAFBRA00707\tPL:Illumina" -M NC000962_3.fasta MAFBRA00707_1_trimmed_paired.fastq MAFBRA00707_2_trimmed_paired.fastq > MAFBRA00707.sam
 
 
 # samtools_faidx_reference_genome <<<<<
@@ -39,7 +39,7 @@ samtools faidx NC000962_3.fasta
 # convert_sam_file_to_bam_file <<<<<
 
 
-samtools view -bt NC000962_3.fasta.fai G04868.sam > G04868.bam
+samtools view -bt NC000962_3.fasta.fai MAFBRA00707.sam > MAFBRA00707.bam
 
 
 
@@ -47,7 +47,7 @@ samtools view -bt NC000962_3.fasta.fai G04868.sam > G04868.bam
 # sort_bam_file <<<<<
 
 
-samtools sort G04868.bam -o G04868.sorted.bam
+samtools sort MAFBRA00707.bam -o MAFBRA00707.sorted.bam
 
 
 
@@ -55,7 +55,7 @@ samtools sort G04868.bam -o G04868.sorted.bam
 # samtools_index_sorted_bam <<<<<
 
 
-samtools index G04868.sorted.bam
+samtools index MAFBRA00707.sorted.bam
 
 
 
@@ -63,7 +63,7 @@ samtools index G04868.sorted.bam
 # mapping_statistics <<<<<
 
 
-samtools flagstat G04868.sorted.bam > G04868_stats.txt
+samtools flagstat MAFBRA00707.sorted.bam > MAFBRA00707_stats.txt
 
 
 
@@ -71,7 +71,7 @@ samtools flagstat G04868.sorted.bam > G04868_stats.txt
 # samtools_mpileup <<<<<
 
 
-samtools mpileup -Q 23 -d 2000 -C 50 -ugf NC000962_3.fasta G04868.sorted.bam | bcftools call -O v -vm -o G04868.raw.vcf
+samtools mpileup -Q 23 -d 2000 -C 50 -ugf NC000962_3.fasta MAFBRA00707.sorted.bam | bcftools call -O v -vm -o MAFBRA00707.raw.vcf
 
 
 
@@ -79,7 +79,7 @@ samtools mpileup -Q 23 -d 2000 -C 50 -ugf NC000962_3.fasta G04868.sorted.bam | b
 # vcfutils_filter<<<<<
 
 
-vcfutils.pl varFilter -d 10 -D 2000 G04868.raw.vcf > G04868.filt.vcf
+vcfutils.pl varFilter -d 10 -D 2000 MAFBRA00707.raw.vcf > MAFBRA00707.filt.vcf
 
 
 
@@ -87,7 +87,7 @@ vcfutils.pl varFilter -d 10 -D 2000 G04868.raw.vcf > G04868.filt.vcf
 # bgzip_filt_file <<<<<
 
 
-bgzip -c G04868.filt.vcf > G04868.filt.vcf.gz
+bgzip -c MAFBRA00707.filt.vcf > MAFBRA00707.filt.vcf.gz
 
 
 
@@ -95,7 +95,7 @@ bgzip -c G04868.filt.vcf > G04868.filt.vcf.gz
 # run_tabix <<<<<
 
 
-tabix -p vcf G04868.filt.vcf.gz
+tabix -p vcf MAFBRA00707.filt.vcf.gz
 
 
 
@@ -103,20 +103,20 @@ tabix -p vcf G04868.filt.vcf.gz
 # snpEff <<<<<
 
 
-java -Xmx4g -jar /opt/snpEff/snpEff.jar -no-downstream -no-upstream -v -c /opt/snpEff/snpEff.config NC000962_3 G04868.filt.vcf > G04868.ann.vcf.gz
+java -Xmx4g -jar /opt/snpEff/snpEff.jar -no-downstream -no-upstream -v -c /opt/snpEff/snpEff.config NC000962_3 MAFBRA00707.filt.vcf > MAFBRA00707.ann.vcf.gz
 
 
 # velveth_assembly <<<<<
 
 
-velveth G04868_41 41 -fastq -shortPaired  G04868_1_trimmed_paired.fastq G04868_1_trimmed_unpaired.fastq -fastq -short G04868_2_trimmed_paired.fastq G04868_2_trimmed_unpaired.fastq
+velveth MAFBRA00707_41 41 -fastq -shortPaired  MAFBRA00707_1_trimmed_paired.fastq MAFBRA00707_1_trimmed_unpaired.fastq -fastq -short MAFBRA00707_2_trimmed_paired.fastq MAFBRA00707_2_trimmed_unpaired.fastq
 
 
 
 # velvetg_produce_graph <<<<<
 
 
-velvetg G04868_41 -exp_cov auto -cov_cutoff auto
+velvetg MAFBRA00707_41 -exp_cov auto -cov_cutoff auto
 
 
 
@@ -124,7 +124,7 @@ velvetg G04868_41 -exp_cov auto -cov_cutoff auto
 # assemblathon_stats <<<<<
 
 
-assemblathon_stats.pl ./G04868_41/contigs.fa
+assemblathon_stats.pl ./MAFBRA00707_41/contigs.fa
 
 
 
@@ -132,7 +132,7 @@ assemblathon_stats.pl ./G04868_41/contigs.fa
 # velveth_assembly <<<<<
 
 
-velveth G04868_49 49 -fastq -shortPaired  G04868_1_trimmed_paired.fastq G04868_1_trimmed_unpaired.fastq -fastq -short G04868_2_trimmed_paired.fastq G04868_2_trimmed_unpaired.fastq
+velveth MAFBRA00707_49 49 -fastq -shortPaired  MAFBRA00707_1_trimmed_paired.fastq MAFBRA00707_1_trimmed_unpaired.fastq -fastq -short MAFBRA00707_2_trimmed_paired.fastq MAFBRA00707_2_trimmed_unpaired.fastq
 
 
 
@@ -140,7 +140,7 @@ velveth G04868_49 49 -fastq -shortPaired  G04868_1_trimmed_paired.fastq G04868_1
 # velvetg_produce_graph <<<<<
 
 
-velvetg G04868_49 -exp_cov auto -cov_cutoff auto
+velvetg MAFBRA00707_49 -exp_cov auto -cov_cutoff auto
 
 
 
@@ -148,7 +148,7 @@ velvetg G04868_49 -exp_cov auto -cov_cutoff auto
 # assemblathon_stats <<<<<
 
 
-assemblathon_stats.pl ./G04868_49/contigs.fa
+assemblathon_stats.pl ./MAFBRA00707_49/contigs.fa
 
 
 
@@ -156,25 +156,25 @@ assemblathon_stats.pl ./G04868_49/contigs.fa
 # velveth_assembly <<<<<
 
 
-velveth G04868_55 55 -fastq -shortPaired  G04868_1_trimmed_paired.fastq G04868_1_trimmed_unpaired.fastq -fastq -short G04868_2_trimmed_paired.fastq G04868_2_trimmed_unpaired.fastq
+velveth MAFBRA00707_55 55 -fastq -shortPaired  MAFBRA00707_1_trimmed_paired.fastq MAFBRA00707_1_trimmed_unpaired.fastq -fastq -short MAFBRA00707_2_trimmed_paired.fastq MAFBRA00707_2_trimmed_unpaired.fastq
 
 # velvetg_produce_graph <<<<<
 
 
-velvetg G04868_55 -exp_cov auto -cov_cutoff auto
+velvetg MAFBRA00707_55 -exp_cov auto -cov_cutoff auto
 
 
 
 # assemblathon_stats <<<<<
 
 
-assemblathon_stats.pl ./G04868_55/contigs.fa
+assemblathon_stats.pl ./MAFBRA00707_55/contigs.fa
 
 
-assemblathon_stats.pl ./G04868_41/contigs.fa
+assemblathon_stats.pl ./MAFBRA00707_41/contigs.fa
 
 
-assemblathon_stats.pl ./G04868_49/contigs.fa
+assemblathon_stats.pl ./MAFBRA00707_49/contigs.fa
 
 
 
@@ -184,34 +184,34 @@ assemblathon_stats.pl ./G04868_49/contigs.fa
 # abacas_align_contigs <<<<<
 
 
-cd G04868_49 &&  cp ../NC000962_3.fasta ./ && abacas.pl -r ../NC000962_3.fasta -q contigs.fa -p promer -b -d -a
+cd MAFBRA00707_49 &&  cp ../NC000962_3.fasta ./ && abacas.pl -r ../NC000962_3.fasta -q contigs.fa -p promer -b -d -a
 
 
 # prokka_annotation <<<<<
 
 
-cd ./G04868_49 && prokka --outdir ./G04868_prokka --prefix G04868 contigs.fa_NC000962_3.fasta.fasta
+cd ./MAFBRA00707_49 && prokka --outdir ./MAFBRA00707_prokka --prefix MAFBRA00707 contigs.fa_NC000962_3.fasta.fasta
 
 
 
 # gzip_compression <<<<<
 
 
-gzip -c G04868_1.fastq > G04868_1.fastq.gz
+gzip -c MAFBRA00707_1.fastq > MAFBRA00707_1.fastq.gz
 
 
 
 # gzip_compression <<<<<
 
 
-gzip -c G04868_2.fastq > G04868_2.fastq.gz
+gzip -c MAFBRA00707_2.fastq > MAFBRA00707_2.fastq.gz
 
 
 
 # snippy_command <<<<<
 
 
-snippy --cpus 4 --outdir G04868 --ref ./NC000962_3.gbk --R1 ./G04868_1.fastq.gz --R2 ./G04868_2.fastq.gz
+snippy --cpus 4 --outdir MAFBRA00707 --ref ./NC000962_3.gbk --R1 ./MAFBRA00707_1.fastq.gz --R2 ./MAFBRA00707_2.fastq.gz
 
 
 
