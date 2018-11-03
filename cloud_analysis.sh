235c572f2aba0e3ffbfc27bb6139ae57e6faf6ea
Abhinav Sharma <abhi18av@users.noreply.github.com>
2018-11-03 07:26:54 +0530

    Update cloud_analysis.sh

--- cloud_analysis.sh
+++ cloud_analysis.sh
@@ -1,3 +1,8 @@
+TODO: Repeat the analysis using the Vagrant image on scaleway servers
+
+https://github.com/BioDragao/ITGE2017_RIO_vagrant
+
+
 MAFBRA00707_R1.fastq
 
 <iframe src="https://onedrive.live.com/embed?cid=F699D7C1FD1AA8A8&resid=F699D7C1FD1AA8A8%216965&authkey=ACPPpCf-Qd81K8o" width="98" height="120" frameborder="0" scrolling="no"></iframe>
