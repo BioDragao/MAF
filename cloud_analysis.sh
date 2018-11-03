caff3d7ce5ad7b8deb7aa5ec68ddabe20e1b59d3
Abhinav Sharma <abhi18av@users.noreply.github.com>
2018-11-03 07:25:56 +0530

    Create cloud_analysis.sh

---
+++ cloud_analysis.sh
@@ -0,0 +1,19 @@
+MAFBRA00707_R1.fastq
+
+<iframe src="https://onedrive.live.com/embed?cid=F699D7C1FD1AA8A8&resid=F699D7C1FD1AA8A8%216965&authkey=ACPPpCf-Qd81K8o" width="98" height="120" frameborder="0" scrolling="no"></iframe>
+
+MAFBRA00707_R2.fastq
+
+<iframe src="https://onedrive.live.com/embed?cid=F699D7C1FD1AA8A8&resid=F699D7C1FD1AA8A8%216966&authkey=ACuK1gPigeI01Wo" width="98" height="120" frameborder="0" scrolling="no"></iframe>
+
+NC000962_3.fasta
+
+<iframe src="https://onedrive.live.com/embed?cid=F699D7C1FD1AA8A8&resid=F699D7C1FD1AA8A8%216883&authkey=AOeKRgbxe6p-qB4" width="98" height="120" frameborder="0" scrolling="no"></iframe>
+
+NC000962_3.gbk
+
+<iframe src="https://onedrive.live.com/embed?cid=F699D7C1FD1AA8A8&resid=F699D7C1FD1AA8A8%216884&authkey=ABgW0YqHWUvl8-s" width="98" height="120" frameborder="0" scrolling="no"></iframe>
+
+NexteraPE-PE.fa
+
+<iframe src="https://onedrive.live.com/embed?cid=F699D7C1FD1AA8A8&resid=F699D7C1FD1AA8A8%216882&authkey=ANWfGV1Fmo-owWM" width="98" height="120" frameborder="0" scrolling="no"></iframe>
