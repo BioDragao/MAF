b4ed99e2063434c3e8bb45ba9e68f181bd6165ff
Abhinav Sharma <abhi18av@users.noreply.github.com>
2018-11-04 01:40:55 +0530

    corrected all wget commands

--- cloud_analysis.sh
+++ cloud_analysis.sh
@@ -9,16 +9,16 @@ wget -O MAFBRA00707_R1.fastq "https://onedrive.live.com/download?cid=F699D7C1FD1
 
 MAFBRA00707_R2.fastq
 
-https://onedrive.live.com/download?cid=F699D7C1FD1AA8A8&resid=F699D7C1FD1AA8A8%216966&authkey=ACuK1gPigeI01Wo
+wget -O MAFBRA00707_R2.fastq "https://onedrive.live.com/download?cid=F699D7C1FD1AA8A8&resid=F699D7C1FD1AA8A8%216966&authkey=ACuK1gPigeI01Wo"
 
 NC000962_3.fasta
 
-https://onedrive.live.com/download?cid=F699D7C1FD1AA8A8&resid=F699D7C1FD1AA8A8%216883&authkey=AOeKRgbxe6p-qB4
+wget -O NC000962_3.fasta "https://onedrive.live.com/download?cid=F699D7C1FD1AA8A8&resid=F699D7C1FD1AA8A8%216883&authkey=AOeKRgbxe6p-qB4"
 
 NC000962_3.gbk
 
-https://onedrive.live.com/download?cid=F699D7C1FD1AA8A8&resid=F699D7C1FD1AA8A8%216884&authkey=ABgW0YqHWUvl8-s
+wget -O NC000962_3.gbk "https://onedrive.live.com/download?cid=F699D7C1FD1AA8A8&resid=F699D7C1FD1AA8A8%216884&authkey=ABgW0YqHWUvl8-s"
 
 NexteraPE-PE.fa
 
-https://onedrive.live.com/download?cid=F699D7C1FD1AA8A8&resid=F699D7C1FD1AA8A8%216882&authkey=ANWfGV1Fmo-owWM
+wget -O NexteraPE-PE.fa "https://onedrive.live.com/download?cid=F699D7C1FD1AA8A8&resid=F699D7C1FD1AA8A8%216882&authkey=ANWfGV1Fmo-owWM"
