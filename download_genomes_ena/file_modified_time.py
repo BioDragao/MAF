787f0e26a41352dbcd72f9056cc1d1e13cc9756b
abhi18av <abhi18av@gmail.com>
2019-04-02 07:33:43 +0000

    pushing from server

--- download_genomes_ena/file_modified_time.py
+++ download_genomes_ena/file_modified_time.py
@@ -25,3 +25,16 @@ def creation_date(path_to_file):
             # We're probably on Linux. No easy way to get creation dates here,
             # so we'll settle for when its content was last modified.
             return stat.st_mtime
+
+###########
+
+import datetime
+mtime = datetime.datetime.fromtimestamp(fname.stat().st_mtime)
+print(mtime)
+
+
+###########
+
+
+ctime = datetime.datetime.fromtimestamp(fname.stat().st_ctime)
+print(ctime)
